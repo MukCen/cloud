0b8657225f3b37c4e1e5f260c2712093e967eef3
MukCen <mukola_1973@ukr.net>
2024-02-17 15:17:19 +0200

    DB CreateUser'

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_URL:
     process.env.DB_URL ||
-    "mongodb+srv://user:user@cluster0.lhohu5k.mongodb.net/cloud/?retryWrites=true&w=majority",
+    "mongodb+srv://user:user@cluster0.lhohu5k.mongodb.net/cloud_cloud",
   SERVER_PORT: process.env.SERVER_PORT || 5000,
 };
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,27 +1,29 @@
 const { Router } = require("express");
+const express = require("express");
 // const { createUser } = require("../controllers/usercontroller");
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 
 const router = new Router();
+// 1 Add router.use(express.json());  and (!errors.isEmpty())
+router.use(express.json());
 
 router.post(
   "/registration",
   [
-    check("email", "Encorect email").isEmail(),
-    check("password", "password mast be longer then 3 symbol").isLength({
-      min: 3,
-      max: 12,
+    check("email", "incorrect email").isEmail(),
+    check("password", "Password must be longer than 3 symbols").isLength({
+      min: 1,
     }),
   ],
   async (req, res) => {
     try {
       const errors = validationResult(req);
-      if (!errors) {
-        res.status(400).json({ messages: "Encorect request", errors });
+      if (!errors.isEmpty()) {
+        res.status(400).json({ messages: "Incorect request", errors });
       }
       const { email, password } = req.body;
-      const candidate = await User.findOne({ email });
+      const candidate = await User.findOne({ email: email });
 
       if (candidate) {
         return res.status(400).json({ message: "User already exists" });
@@ -30,7 +32,7 @@ router.post(
       const newUser = new User({ email, password });
       await newUser.save();
 
-      // return res.status(201).json({ message: "User created successfully" });
+      return res.status(201).json({ message: "User created successfully" });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Server error" });
